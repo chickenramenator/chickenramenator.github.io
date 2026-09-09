a85251190b909be72c072b6d5c7f52dc7d1c5735
chickenramenator <chickenrqmen@gmail.com>
2026-09-09 16:31:20 +0000

    worked on adding platforms and a collectable

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,16 +27,20 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(600, 610, 200, 20)
+createPlatform(325, 490, 100, 20,)
+createPlatform(1050, 490, 200, 20)
+createPlatform(100, 400, 150, 20)
+createPlatform(500, 300, 100, 20)
+createPlatform(800, 200, 200, 20)
+createPlatform(1250, 200, 200, 20)
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 100, 350)
 
 
     
